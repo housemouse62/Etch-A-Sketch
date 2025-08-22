160ceb0a15dfea58fa151503bbf30fc6ea87ef2d
housemouse62 <ryan.skeels@gmail.com>
2025-08-23 01:16:18 +0300

    it's a start. too many square. added a class for one to change the css for all. continue with other squares. i know i can create them with a loop, but for now i'm doing it manually.

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector("#container")
+
+
+
+// all squares
+const square1 = document.createElement("div");
+square1.classList.add('square');
+const square2 = document.createElement("div", "square");
+const square3 = document.createElement("div", "square");
+const square4 = document.createElement("div", "square");
+const square5 = document.createElement("div", "square");
+const square6 = document.createElement("div", "square");
+const square7 = document.createElement("div", "square");
+const square8 = document.createElement("div", "square");
+const square9 = document.createElement("div", "square");
+const square10 = document.createElement("div", "square");
+const square11 = document.createElement("div", "square");
+const square12 = document.createElement("div", "square");
+const square13 = document.createElement("div", "square");
+const square14 = document.createElement("div", "square");
+const square15 = document.createElement("div", "square");
+const square16 = document.createElement("div", "square");
+const squaresAll = document.querySelectorAll("div")
+
+square1.textContent = "hello world";
+square1.style.border = "solid black";
+container.appendChild(square1);
+
