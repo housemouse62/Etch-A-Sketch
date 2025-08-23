2419edaaad595bb93a6f0f9c6ee13aa07ee774dc
housemouse62 <ryan.skeels@gmail.com>
2025-08-23 23:47:38 +0300

    Grid prompt is working. Need to add a button to reset the board and a max # for the prompt.

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+
+function numberOfSquares() {
+const numberOfSquaresPerSidePrompt = prompt("How many squares per side do you want for your Etch-A-Sketch?", 0);
+if (numberOfSquaresPerSidePrompt > 100) {
+    alert("Don't get greedy, choose a number of less than 100 please");
+    numberOfSquares();
+} else {
+    const numberOfSquaresPerSide = numberOfSquaresPerSidePrompt;
+    return;
+}
+}
+
+numberOfSquares();
 const container = document.querySelector("#container");
 
-const squareHeightAndWidth = (960 / 4) + "px";
-console.log(squareHeightAndWidth);
+const squareHeightAndWidth = (960 / numberOfSquaresPerSide) + "px";
 
 const createDivs = (parent) => {
     const newSquare = document.createElement("div");
     newSquare.style.width = squareHeightAndWidth;
     newSquare.style.height = squareHeightAndWidth;
-    newSquare.style.backgroundColor = "#000";
+    newSquare.style.backgroundColor = "lightblue";
     newSquare.classList.add("square");
     parent.append(newSquare);
+
+    newSquare.addEventListener("mouseover", () => {
+    newSquare.style.backgroundColor = "white";
+    });
+
+    newSquare.addEventListener("mouseout", () => {
+    newSquare.style.backgroundColor = "lightblue";
+    });
+
     console.log(newSquare);
 }
 
-//createDivs(container)
+for (let i = 1; i <= (numberOfSquaresPerSide * numberOfSquaresPerSide); i++) {
+    createDivs(container);
+}
+
+const newSquare = document.querySelector(".square");
+
+
+
+console.log(newSquare)
