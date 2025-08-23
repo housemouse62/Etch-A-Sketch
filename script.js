3fe5baa58d9d342d634f534f86ecf8c63da971fc
housemouse62 <ryan.skeels@gmail.com>
2025-08-23 22:37:16 +0300

    started over. coded for 1 square here.

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
+const squareHeightAndWidth = (960 / 4) + "px";
+console.log(squareHeightAndWidth);
 
+const createDivs = (parent) => {
+    const newSquare = document.createElement("div");
+    newSquare.style.width = squareHeightAndWidth;
+    newSquare.style.height = squareHeightAndWidth;
+    newSquare.style.backgroundColor = "#000";
+    newSquare.classList.add("square");
+    parent.append(newSquare);
+    console.log(newSquare);
+}
 
-// all squares
-const square1 = document.createElement("div");
-square1.classList.add('square');
-const square2 = document.createElement("div", "square");
-const square3 = document.createElement("div", "square");
-const square4 = document.createElement("div", "square");
-const square5 = document.createElement("div", "square");
-const square6 = document.createElement("div", "square");
-const square7 = document.createElement("div", "square");
-const square8 = document.createElement("div", "square");
-const square9 = document.createElement("div", "square");
-const square10 = document.createElement("div", "square");
-const square11 = document.createElement("div", "square");
-const square12 = document.createElement("div", "square");
-const square13 = document.createElement("div", "square");
-const square14 = document.createElement("div", "square");
-const square15 = document.createElement("div", "square");
-const square16 = document.createElement("div", "square");
-const squaresAll = document.querySelectorAll("div")
-
-square1.textContent = "hello world";
-square1.style.border = "solid black";
-container.appendChild(square1);
-
+//createDivs(container)
