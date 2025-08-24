4e26f55792e34cfe133c48a3a0d266a3fb5ed952
housemouse62 <ryan.skeels@gmail.com>
2025-08-24 22:50:46 +0300

    Finished with some help rearranging the code. Things to do: 1. Make it more like MS Paint using mousedown 2. Have color change options 3. Check out the Extra Credit option on Odin

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 let squaresPerSide;
 
+const container = document.querySelector("#container");
+
+const initGame = () => {
+    const startGame = confirm("Are you sure you would like to use this Etch-A-Sketch?");
+    startGame ? userPrompt() : alert('Well alrighty then, come back soon');
+};
+
+
 const userPrompt = () => {
 while (true) {
-    squaresPerSide = prompt("How many squares per side do you want for your Etch-A-Sketch?\nNo higher than 100 please, we don't wanna crash the system!", 0);
+    squaresPerSide = parseInt(prompt("How many squares per side do you want for your Etch-A-Sketch?\nNo higher than 100 please, we don't wanna crash the system!", 0), 10);
 if (squaresPerSide > 100) {
     alert("Number too high");
     continue;
 } else {
-    return;
+    createBoard(squaresPerSide);
+    break;
 }
 }};
-userPrompt();
 
-const container = document.querySelector("#container");
+const createBoard = (squaresPerSide) => {
+    container.innerHTML = "";
+    const oneSquareSide = (960 / squaresPerSide) + "px";
 
-const oneSquareSide = (960 / squaresPerSide) + "px";
+    for (let i = 1; i <= (squaresPerSide * squaresPerSide); i++) {
+    createDivs(container, oneSquareSide);
+}
+};
 
-const createDivs = (parent) => {
+const createDivs = (parent, size) => {
     const newSquare = document.createElement("div");
-    newSquare.style.width = oneSquareSide;
-    newSquare.style.height = oneSquareSide;
+    newSquare.style.width = size;
+    newSquare.style.height = size;
     newSquare.style.backgroundColor = "lightblue";
     newSquare.classList.add("square");
     parent.append(newSquare);
@@ -27,17 +40,17 @@ const createDivs = (parent) => {
     newSquare.addEventListener("mouseover", () => {
     newSquare.style.backgroundColor = "white";
     });
-
-//    newSquare.addEventListener("mouseout", () => {
-//    newSquare.style.backgroundColor = "lightblue";
-//   });
-}
-
-for (let i = 1; i <= (squaresPerSide * squaresPerSide); i++) {
-    createDivs(container);
- }
+ };
 
 const newSquare = document.querySelector(".square");
 
 
+const newBoard = document.createElement('button');
+newBoard.innerText = 'New Board';
+container.parentNode.insertBefore(newBoard, container);
 
+newBoard.addEventListener('click', () => {
+userPrompt();
+});
+
+initGame();
